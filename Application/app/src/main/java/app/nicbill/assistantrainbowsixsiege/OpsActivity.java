7bd62ecda5d05399cbc95663c320f29730f66aeb
Nicolas Bilodeau <nicbill_7_@hotmail.com>
2018-03-20 20:17:48 -0400

    Création app android/Ajout mainMenu/CréationLogo

---
+++ Application/app/src/main/java/app/nicbill/assistantrainbowsixsiege/OpsActivity.java
@@ -0,0 +1,13 @@
+package app.nicbill.assistantrainbowsixsiege;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ops);
+    }
+}
